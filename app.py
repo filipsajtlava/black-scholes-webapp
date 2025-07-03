79b44b2ab977c1e02766055cf5d86ae4450155f5
Filip <sajtlavafilip@gmail.com>
2025-07-03 17:06:58 +0200

    Added the pricing formula and a skeleton for the web application

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("soon_TM")
+from pricing import black_scholes_price
+import streamlit as st
+
+st.title("Black-Scholes Option Pricing")
+
+st.sidebar.header("Option Parameters")
+
+S = st.sidebar.number_input("Stock price (S) in €", min_value=0.01, value=100.0, max_value=250.0, step = 5.0)
+K = st.sidebar.number_input("Strike price (K) in €", min_value=0.01, value=100.0, max_value=250.0, step = 5.0)
+T = st.sidebar.number_input("Time to maturity (T) in years", min_value=0.01, value=1.0, max_value=5.0, step = 0.05)
+r = st.sidebar.number_input("Non-risk interest rate (r)", min_value=0.0, value=0.05, step = 0.01)
+sigma = st.sidebar.number_input("Volatility (σ)", min_value=0.01, max_value=1.0, value=0.1, step = 0.01)
+option_type = st.sidebar.selectbox("Option Type", ["Call", "Put"])
+
+if st.button("Calculate option price"):
+    price = black_scholes_price(S, K, T, r, sigma, option_type)
+    st.success(f"{option_type} option price: {price:.2f}€")
---
+++ pricing.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.stats import norm
+
+def black_scholes_price(S, K, T, sigma, r, option_type = "Call"):
+
+    # Parameters:
+
+    # S - price of underlying asset (for example - stocks)
+    # K - strike price of the option
+    # T - time until maturity in years (with the assumption of an european option)
+    # sigma - volatility of the underlying asset
+    # r - non-risk interest rate (used for discounting from T to t_0)
+
+    d1 = (np.log(S / K) + (r + sigma**2 * 0.5) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    if option_type == "Call":
+        price = S * norm.cdf(d1) - K * np.exp(-r) * norm.cdf(d2)
+    elif option_type == "Put":
+        price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+    else:
+        raise ValueError("option_type must be either 'Call' or 'Put'")
+    
+    return price
